d25c098752969b7a6db2b6f8eb368e79fdf5bf18
Cara Hill <cara@rabidtech.co.nz>
2018-11-21 11:58:31 +1300

    Third test passing.

--- word-count.js
+++ word-count.js
@@ -4,7 +4,11 @@ class Words {
     let wordsArray = words.split(" ");
     let countObject = {};
     wordsArray.map(word => {
-      countObject[word] = 1;
+      if (word in countObject) {
+        countObject[word] = countObject[word] +1;
+      } else {
+        countObject[word] = 1;
+      }
     });
     return countObject;
   }
