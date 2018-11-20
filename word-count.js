18a827c1b5a544f3717fb3bd13b849c3ef43b07d
Cara Hill <cara@rabidtech.co.nz>
2018-11-21 11:05:34 +1300

    Second test passing.

--- word-count.js
+++ word-count.js
@@ -1,7 +1,12 @@
 class Words {
 
-  count(word) {
-    return { word: 1 }
+  count(words) {
+    let wordsArray = words.split(" ");
+    let countObject = {};
+    wordsArray.map(word => {
+      countObject[word] = 1;
+    });
+    return countObject;
   }
 }
 
